41c97ddbebb3cb873424b2ef6af3d88e5a97734a
mrange <marten_range@hotmail.com>
2013-09-07 00:10:34 +0200

    1. RayTracer basic structure inplace

--- fun/RayTracer/RayTracer/Program.fs
+++ fun/RayTracer/RayTracer/Program.fs
@@ -1,9 +1,18 @@
 ﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+open System.Windows.Media.Imaging
+open System.Windows.Threading
 
 [<AutoOpen>]
 module Util =
     let sign d = if d < 0. then -1. else 1.
     let pi = Math.PI
+    let pi2 = 2. * pi
+
+    let degree2rad d = d / pi2
+    let rad2degree r = r * pi2
 
     let clamp x min max = 
         if x < min then min
@@ -12,7 +21,12 @@ module Util =
 
     let norm x = clamp x -1. 1.
     let unorm x = clamp x 0. 1.
-            
+
+    let dispatch (d : Dispatcher) (a : unit -> unit) = 
+        let a' = Action a
+        ignore <| d.BeginInvoke (DispatcherPriority.ApplicationIdle, a')
+        
+    let asByte d = byte ((unorm d) * 255.)
 
 type Color = 
     {
@@ -21,6 +35,10 @@ type Color =
         Blue    : double
     }
     static member New red green blue = {Red = unorm red; Green = unorm green; Blue = unorm blue}
+    member x.Dim t = Color.New (t * x.Red) (t * x.Green) (t * x.Blue)
+
+    
+
 
 type Material = 
     {
@@ -46,6 +64,7 @@ type Vector2 =
     static member (*) (x : Vector2, y : Vector2) = x.X * y.X + x.Y * y.Y
 
     member x.Scale s    = Vector2.New (s * x.X) (s * x.Y)
+    member x.Normalize  = x.Scale (1. / x.L1)
     member x.L2         = x * x
     member x.L1         = sqrt x.L2
     member x.Min y      = Vector2.New (min x.X y.X) (min x.Y y.Y)
@@ -64,9 +83,10 @@ type Vector3 =
     static member (+) (x : Vector3, y : Vector3) = Vector3.New (x.X + y.X) (x.Y + y.Y) (x.Z + y.Z)
     static member (-) (x : Vector3, y : Vector3) = Vector3.New (x.X - y.X) (x.Y - y.Y) (x.Z - y.Z)
     static member (*) (x : Vector3, y : Vector3) = x.X * y.X + x.Y * y.Y + x.Z * y.Z
-    static member ( *** ) (x : Vector3, y : Vector3) = Vector3.New (x.Y * y.Z - x.Z * y.Y) (x.Z * y.Y - x.Y*y.Z) (x.X * y.Y - x.Y * y.X)
+    static member ( *+* ) (x : Vector3, y : Vector3) = Vector3.New (x.Y * y.Z - x.Z * y.Y) (x.Z * y.Y - x.Y*y.Z) (x.X * y.Y - x.Y * y.X)
 
     member x.Scale s    = Vector3.New (s * x.X) (s * x.Y) (s * x.Z)
+    member x.Normalize  = x.Scale (1. / x.L1)
     member x.L2         = x * x
     member x.L1         = sqrt x.L2
     member x.Min y      = Vector3.New (min x.X y.X) (min x.Y y.Y) (min x.Z y.Z)
@@ -74,10 +94,37 @@ type Vector3 =
 
 type ViewPort = 
     {
-        Corner          : Vector3
-        OppositeCorner  : Vector3
+        Center          : Vector3
+        Normal          : Vector3
+        Axis0           : Vector3
+        Axis1           : Vector3
+        Width           : double
+        Height          : double
+        Corner0         : Vector3
+        Corner1         : Vector3
+        Corner2         : Vector3
+        Corner3         : Vector3
     }
-    static member New (c :Vector3) (oc : Vector3) = {Corner = c.Min oc; OppositeCorner = c.Max oc}
+
+    static member New (c :Vector3) (normal : Vector3) (up : Vector3) width height = 
+        let xaxis = (up *+* normal).Normalize
+        let yaxis = (up *+* xaxis).Normalize
+
+        let halfx       = xaxis.Scale (width / 2.)
+        let halfy       = yaxis.Scale (height / 2.)
+
+        {
+            Center  = c
+            Normal  = normal.Normalize
+            Axis0   = xaxis
+            Axis1   = yaxis
+            Width   = width
+            Height  = height
+            Corner0 = c - halfx - halfy
+            Corner1 = c + halfx - halfy
+            Corner2 = c + halfx + halfy
+            Corner3 = c - halfx + halfy
+        }
 
 
 type Surface = Vector2 -> Material
@@ -118,11 +165,11 @@ type Sphere (surface: Surface, center : Vector3, radius : double) =
 
     member x.NormalAndMaterial p =
         let n' = (p - center) 
-        let n = n'.Scale (1. / (n'.L1))
+        let n = n'.Normalize
         let y' = Vector3.New n.X center.Y n.Z
-        let y = (sign (n.Y - center.Y)) * acos ((y' * n) / (y'.L1 * n.L1)) / (2. * pi) + 0.5
+        let y = (sign (n.Y - center.Y)) * acos ((y' * n) / (y'.L1 * n.L1)) / pi2 + 0.5
         let x' = Vector3.New n.X center.Y center.Z
-        let x = (sign (n.Z - center.Z)) * acos ((x' * y') / (x'.L1 * y'.L1)) / (2. * pi) + 0.5
+        let x = (sign (n.Z - center.Z)) * acos ((x' * y') / (x'.L1 * y'.L1)) / pi2 + 0.5
 
         let m = base.Surface <| Vector2.New x y
 
@@ -154,23 +201,18 @@ type Sphere (surface: Surface, center : Vector3, radius : double) =
 type Plane (surface: Surface, offset : double, normal : Vector3)=
     inherit Shape (surface)
 
-    let N = 
-        let n = normal.L1
-        if n <> 0. then normal.Scale (1. / n)
-        else Vector3.Zero
+    let N = normal.Normalize
 
     let X = 
         
-        if N.X <> 0. then Vector3.New (offset / N.X) 0. 0.
-        elif N.Y <> 0. then Vector3.New 0. (offset / N.Y) 0.
-        elif N.Z <> 0. then Vector3.New 0. 0. (offset / N.Z)
-        else Vector3.Zero
+        let result = 
+            if N.X <> 0. then Vector3.New (offset / N.X) 0. 0.
+            elif N.Y <> 0. then Vector3.New 0. (offset / N.Y) 0.
+            elif N.Z <> 0. then Vector3.New 0. 0. (offset / N.Z)
+            else Vector3.Zero
+        result.Normalize
 
-    let Y = 
-        let n = N.L1
-        let x = normal.L1
-        if n <> 0. && x <> 0. then (N *** X).Scale (1. / x) // n is either 1 or 0
-        else Vector3.Zero
+    let Y = N *+* X
 
     override x.Intersect r = 
         let t = -(r.Origin*N + offset) / (r.Direction*normal)
@@ -195,6 +237,7 @@ let Matte c = Material.New c 1. 1. 0. 0.
 let UniformSurface (material : Material) : Surface = fun v -> material
 
 [<EntryPoint>]
+[<STAThread>]
 let main argv = 
 
     let lights = 
@@ -209,10 +252,70 @@ let main argv =
         ]
 
     let eye         = Vector3.New 5. 1. 1.
-    let viewPort    = ViewPort.New (Vector3.New 4. 0. 0.) (Vector3.New 6. 2. 2.)
+    let at          = Vector3.New 0. 0. 0.
+    let clipDistance= 1.
+    let clipNormal  = (at - eye).Normalize
+    let clipUp      = Vector3.New 0. 1. 0.
+    let clipCenter  = eye + clipNormal.Scale clipDistance
+    let fov         = degree2rad 90.
 
+    let ambientLight = White.Dim 0.75
+
+   
+    let window = new Window()
+    window.MinWidth <- 640.
+    window.MinHeight <- 400.
+
+
+    use loaded = window.Loaded.Subscribe (fun v -> 
+        let width   = window.Width
+        let height  = window.Height
+        let ratio   = width / height
+
+        let wb = new WriteableBitmap(int width, int height, 96., 96., PixelFormats.Bgr32, null)
+
+        let iwidth = wb.PixelWidth
+        let iheight = wb.PixelHeight
+
+        let viewPortDim = clipDistance / tan (fov / 2.)
+        let viewPortWidth, viewPortHeight = 
+            if width > height then
+                viewPortDim, viewPortDim / ratio
+            else
+                viewPortDim * ratio, viewPortDim
+
+        let viewPort = ViewPort.New clipCenter clipNormal clipUp viewPortWidth viewPortHeight
+
+        let tracer = 
+            async {
+                
+                let row = [| for i in 0..iheight - 1 -> Black|]
+
+                for x in 0..iwidth - 1 do
+                    for y in 0..iheight - 1 do
+                        row.[y] <- White
     
+                    dispatch window.Dispatcher (fun () -> 
+                        let pixels = 
+                            [| for i in row do
+                                yield byte 255
+                                yield asByte i.Red 
+                                yield asByte i.Green
+                                yield asByte i.Blue
+                            |]
+                        wb.WritePixels (Int32Rect (x, 0, 1, iheight), pixels, 4, 0)
+                        )
 
+                return ()
+            }
 
+        Async.Start tracer
+
+        let i = new Image ()
+        i.Source <- wb
+        window.Content <- i
+        )
+
+    let result = window.ShowDialog()
 
     0
