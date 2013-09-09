9a80d017ffd211c347411885a91881ce41ac8f71
mrange <marten_range@hotmail.com>
2013-09-09 21:55:41 +0200

    1. Added OctTree start

---
+++ fun/RayTracer/RayTracer/OctTree.fs
@@ -0,0 +1,38 @@
+﻿namespace RayTracer
+
+type BoundingPlane = 
+    {
+        Normal  : Vector3
+        Offset  : float
+    }
+
+type BoundingBox = 
+    {
+        Up          : BoundingPlane
+        Down        : BoundingPlane
+        North       : BoundingPlane
+        South       : BoundingPlane
+        East        : BoundingPlane
+        West        : BoundingPlane
+    }
+
+type OctTree<'a> =
+    | Leaf      of 'a list
+    | Box       of BoundingBox * Fork<'a>
+    member x.FindCandidates (r : Ray) = 
+        match x with
+        |   Leaf c  -> c
+        |   _       -> []
+and Fork<'a> =
+    {
+        UpNorthEast     : OctTree<'a>
+        UpNorthWest     : OctTree<'a>
+        UpSouthWest     : OctTree<'a>
+        UpSouthEast     : OctTree<'a>
+        DownNorthEast   : OctTree<'a>
+        DownNorthWest   : OctTree<'a>
+        DownSouthWest   : OctTree<'a>
+        DownSouthEast   : OctTree<'a>
+    }
+    
+        
