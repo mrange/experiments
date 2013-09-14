d1f4df152c7a68d36daf5ac4f466c95428447a21
mrange <marten_range@hotmail.com>
2013-09-14 15:28:31 +0200

    1. Added new norms 2. Added Lerp

--- fun/RayTracer/RayTracer/LinearAlgebra.fs
+++ fun/RayTracer/RayTracer/LinearAlgebra.fs
@@ -18,11 +18,14 @@ type Vector2 =
     static member (*) (x : Vector2, y : Vector2) = x.X * y.X + x.Y * y.Y
 
     member x.Scale s    = Vector2.New (s * x.X) (s * x.Y)
-    member x.Normalize  = x.Scale (1. / x.L1)
+    member x.Normalize  = x.Scale (1. / x.Length)
+    member x.L1         = abs (x.X) + abs (x.Y)
     member x.L2         = x * x
-    member x.L1         = sqrt x.L2
+    member x.LInf       = max (abs x.X) (abs x.Y)
+    member x.Length     = sqrt x.L2
     member x.Min y      = Vector2.New (min x.X y.X) (min x.Y y.Y)
     member x.Max y      = Vector2.New (max x.X y.X) (max x.Y y.Y)
+    member x.Lerp y t   = x + (y - x).Scale t
 
     member x.Reflect (n : Vector2)  = (n.Scale (2.*(x * n))) - x
 
@@ -53,11 +56,14 @@ type Vector3 =
     static member ( *+* ) (x : Vector3, y : Vector3) = Vector3.New (x.Y * y.Z - x.Z * y.Y) (x.Z * y.X - x.X * y.Z) (x.X * y.Y - x.Y * y.X)
 
     member x.Scale s    = Vector3.New (s * x.X) (s * x.Y) (s * x.Z)
-    member x.Normalize  = x.Scale (1. / x.L1)
+    member x.Normalize  = x.Scale (1. / x.Length)
+    member x.L1         = abs (x.X) + abs (x.Y) + abs (x.Z)
     member x.L2         = x * x
-    member x.L1         = sqrt x.L2
+    member x.LInf       = max (max (abs x.X) (abs x.Y)) (abs x.Z)
+    member x.Length     = sqrt x.L2
     member x.Min y      = Vector3.New (min x.X y.X) (min x.Y y.Y) (min x.Z y.Z)
     member x.Max y      = Vector3.New (max x.X y.X) (max x.Y y.Y) (max x.Z y.Z)
+    member x.Lerp y t   = x + (y - x).Scale t
 
     member x.Reflect (n : Vector3)  = x - (n.Scale (2.*(x * n)))
 
