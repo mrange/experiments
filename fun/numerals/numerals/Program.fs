444fac80e8963c130cb54bd5a825b1bf697ee5d0
mrange <marten_range@hotmail.com>
2013-09-17 21:29:36 +0200

    1. Added Sub 2. Added DivMod

--- fun/numerals/numerals/Program.fs
+++ fun/numerals/numerals/Program.fs
@@ -1,4 +1,6 @@
 ﻿
+[<StructuralEquality>]
+[<StructuralComparison>]
 type Numeral = 
     |   Zero
     |   S of Numeral
@@ -11,13 +13,10 @@ type Numeral =
 
 let Succ n  = S n
 let Fold f seed (n : Numeral) = n.Fold f seed
-
-let Add l r = Fold Succ l r
-let Mul l r = Fold (Add l) Zero r
-
-let inline ( + ) (l : Numeral) (r : Numeral)    = Add l r
-let inline ( * ) (l : Numeral) (r : Numeral)    = Mul l r
-
+let rec Until e n v = 
+    if e v then v
+    else Until e n (n v)
+        
 let Zero    = Zero
 let One     = Succ Zero
 let Two     = Succ One
@@ -29,15 +28,55 @@ let Seven   = Succ Six
 let Eight   = Succ Seven
 let Nine    = Succ Eight
                     
+let Add l r = Fold Succ l r
+let Mul l r = Fold (Add l) Zero r
+let Sub l r = 
+    match l,r with
+    | _     when l < r  -> Zero
+    | _                 -> 
+        let _,n = Until (fun (rem,_) -> rem = l) (fun (rem,n) -> (Succ rem, Succ n)) (r, Zero)
+        n
+
+// Not done yet
+let DivMod l r = 
+    match l,r with
+    | _,Zero                -> Zero,Zero
+    | _     when l < r      -> Zero,r
+    | _                     -> 
+        let _,n = Until (fun (acc,_) -> acc > l) (fun (acc,n) -> (Add acc r, Succ n)) (Zero, Zero)
+        (Sub n One), Zero
+
+let inline ( + ) (l : Numeral) (r : Numeral)    = Add l r
+let inline ( - ) (l : Numeral) (r : Numeral)    = Sub l r
+let inline ( * ) (l : Numeral) (r : Numeral)    = Mul l r
+
 let TestAdd l r = 
     let result = l + r
     printf "%s + %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
+
+let TestSub l r = 
+    let result = l - r
+    printf "%s - %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
 
 let TestMul l r = 
     let result = l * r
     printf "%s * %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
     
+let TestDm l r = 
+    let n,rem = DivMod l r
+    printf "%s / %s = %s,%s\n" (l.ToString ()) (r.ToString ()) (n.ToString ()) (rem.ToString ())
+    
+let TestEq l r = 
+    let result = l = r
+    printf "%s = %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
 
+let TestLt l r = 
+    let result = l < r
+    printf "%s < %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
+
+let TestGt l r = 
+    let result = l > r
+    printf "%s > %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
 
 [<EntryPoint>]
 let main argv = 
@@ -52,6 +91,15 @@ let main argv =
     TestAdd Two     Two 
     TestAdd Seven   Five
 
+    TestSub Zero    Two 
+    TestSub One     Two 
+    TestSub Two     Two 
+    TestSub Seven   Five
+    
+    TestSub Two     Zero    
+    TestSub Two     One     
+    TestSub Two     Two     
+    TestSub Five    Seven   
     
     TestMul Two     Zero
     TestMul Two     One
@@ -62,5 +110,25 @@ let main argv =
     TestMul One     Two 
     TestMul Two     Two 
     TestMul Seven   Five
+
+    TestDm  Eight   Two     
+    TestDm  Eight   One
+    TestDm  Eight   Three
+    TestDm  Eight   Four
+
+    TestEq  Zero    Zero
+    TestEq  Zero    Two
+    TestEq  Two     Zero
+    TestEq  Two     Two
+
+    TestLt  Zero    Zero
+    TestLt  Zero    Two
+    TestLt  Two     Zero
+    TestLt  Two     Two
+
+    TestGt  Zero    Zero
+    TestGt  Zero    Two
+    TestGt  Two     Zero
+    TestGt  Two     Two
 
     0
