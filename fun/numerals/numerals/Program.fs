4923a356802869f3516a84be837ab5ed0cd5eae8
mrange <marten_range@hotmail.com>
2013-10-02 18:10:54 +0200

    1. Uses the peano definition of numerals, additions and multiplication

--- fun/numerals/numerals/Program.fs
+++ fun/numerals/numerals/Program.fs
@@ -3,20 +3,14 @@
 [<StructuralComparison>]
 type Numeral = 
     |   Zero
-    |   S of Numeral
-    member x.Fold f seed = 
-        match x with
-        |   Zero    -> seed
-        |   S n'    -> f (n'.Fold f seed) 
-    member x.ToInteger () = x.Fold (fun i -> i + 1) 0
-    override x.ToString () = (x.ToInteger ()).ToString ()
+    |   Succ of Numeral
+    override x.ToString () =
+        let rec Length (n : Numeral) = 
+            match n with
+            |   Zero    -> 0
+            |   Succ p  -> 1 + Length p
+        (Length x).ToString ()
 
-let Succ n  = S n
-let Fold f seed (n : Numeral) = n.Fold f seed
-let rec Until e n v = 
-    if e v then v
-    else Until e n (n v)
-        
 let Zero    = Zero
 let One     = Succ Zero
 let Two     = Succ One
@@ -28,56 +22,39 @@ let Seven   = Succ Six
 let Eight   = Succ Seven
 let Nine    = Succ Eight
                     
-let Add l r = Fold Succ l r
-let Mul l r = Fold (Add l) Zero r
-let Sub l r = 
-    match l,r with
-    | _     when l < r  -> Zero
-    | _                 -> 
-        let _,n = Until (fun (rem,_) -> rem = l) (fun (rem,n) -> (Succ rem, Succ n)) (r, Zero)
-        n
-
-// Not done yet
-let DivMod l r = 
-    match l,r with
-    | _,Zero                -> Zero,Zero
-    | _     when l < r      -> Zero,r
-    | _                     -> 
-        let _,n = Until (fun (acc,_) -> acc > l) (fun (acc,n) -> (Add acc r, Succ n)) (Zero, Zero)
-        (Sub n One), Zero
+let rec Add l r = 
+    match r with
+    |   Zero        -> l
+    |   Succ p      -> Succ (Add l p)
 
 let inline ( + ) (l : Numeral) (r : Numeral)    = Add l r
-let inline ( - ) (l : Numeral) (r : Numeral)    = Sub l r
+
+let rec Mul l r = 
+    match r with
+    |   Zero        -> Zero
+    |   Succ p      -> l + (Mul l p)
+
 let inline ( * ) (l : Numeral) (r : Numeral)    = Mul l r
+
+let rec Pow l r = 
+    match r with
+    |   Zero        -> One
+    |   Succ p      -> l * (Pow l p)
+
+let inline ( ^^^ ) (l : Numeral) (r : Numeral)    = Pow l r
 
 let TestAdd l r = 
     let result = l + r
     printf "%s + %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
 
-let TestSub l r = 
-    let result = l - r
-    printf "%s - %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
-
 let TestMul l r = 
     let result = l * r
     printf "%s * %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
     
-let TestDm l r = 
-    let n,rem = DivMod l r
-    printf "%s / %s = %s,%s\n" (l.ToString ()) (r.ToString ()) (n.ToString ()) (rem.ToString ())
+let TestPow l r = 
+    let result = l ^^^ r
+    printf "%s ^^^ %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
     
-let TestEq l r = 
-    let result = l = r
-    printf "%s = %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
-
-let TestLt l r = 
-    let result = l < r
-    printf "%s < %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
-
-let TestGt l r = 
-    let result = l > r
-    printf "%s > %s = %s\n" (l.ToString ()) (r.ToString ()) (result.ToString ())
-
 [<EntryPoint>]
 let main argv = 
 
@@ -91,16 +68,6 @@ let main argv =
     TestAdd Two     Two 
     TestAdd Seven   Five
 
-    TestSub Zero    Two 
-    TestSub One     Two 
-    TestSub Two     Two 
-    TestSub Seven   Five
-    
-    TestSub Two     Zero    
-    TestSub Two     One     
-    TestSub Two     Two     
-    TestSub Five    Seven   
-    
     TestMul Two     Zero
     TestMul Two     One
     TestMul Two     Two
@@ -111,24 +78,10 @@ let main argv =
     TestMul Two     Two 
     TestMul Seven   Five
 
-    TestDm  Eight   Two     
-    TestDm  Eight   One
-    TestDm  Eight   Three
-    TestDm  Eight   Four
-
-    TestEq  Zero    Zero
-    TestEq  Zero    Two
-    TestEq  Two     Zero
-    TestEq  Two     Two
-
-    TestLt  Zero    Zero
-    TestLt  Zero    Two
-    TestLt  Two     Zero
-    TestLt  Two     Two
-
-    TestGt  Zero    Zero
-    TestGt  Zero    Two
-    TestGt  Two     Zero
-    TestGt  Two     Two
+    TestPow Zero    Two 
+    TestPow One     Two 
+    TestPow Two     Zero
+    TestPow Two     Two
+    TestPow Four    Three
 
     0
