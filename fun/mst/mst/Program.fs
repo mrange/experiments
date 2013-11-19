70e6ebc4038f740ba64361f9c1543e1ecfdfef93
mrange <marten_range@hotmail.com>
2013-11-19 21:27:30 +0100

    1. Adding L-system

--- fun/mst/mst/Program.fs
+++ fun/mst/mst/Program.fs
@@ -1,9 +1,46 @@
 ﻿open mst
 
 open System.Windows
+open System.Windows.Media
 open System.Windows.Automation
 
 open mst.lowlevel
+
+module LSystem =
+    type Alphabet =
+        | DoNothing     // X
+        | Forward       // F
+        | TurnLeft      // -
+        | TurnRight     // +
+        | Store         // [
+        | Restore       // ]
+
+    let Angle = 25.
+
+    let Initial = [DoNothing]
+
+    let Rule (a : Alphabet) = 
+        match a with
+                     // F       -        [     [     X         ]       +         X         ]       +         F       [     +         F       X         ]       -        X 
+        | DoNothing -> [Forward;TurnLeft;Store;Store;DoNothing;Restore;TurnRight;DoNothing;Restore;TurnRight;Forward;Store;TurnRight;Forward;DoNothing;Restore;TurnLeft;DoNothing]
+                     // F       F
+        | Forward   -> [Forward;Forward]
+        | _         -> [a]
+
+    let rec Generate =  function 
+                        | 0 ->  Initial         
+                        | n ->  let previous = Generate (n - 1)
+                                previous |> List.collect Rule
+
+    let RotateLeft  =   let m = Matrix.Identity
+                        m.Rotate(Angle)
+                        m
+
+    let RotateRight =   let m = Matrix.Identity
+                        m.Rotate(-Angle)
+                        m
+
+
 
 [<EntryPoint>]
 let main argv = 
