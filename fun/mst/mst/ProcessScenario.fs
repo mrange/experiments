84dae4b5e2cdd0d1936d2c1b6f215213ea50af9b
mrange <marten_range@hotmail.com>
2013-11-19 13:20:01 +0100

    1. Code styles fixed

--- fun/mst/mst/ProcessScenario.fs
+++ fun/mst/mst/ProcessScenario.fs
@@ -8,19 +8,19 @@ module ProcessScenario =
 
     let StartProcess (exePath : string) : Scenario<int> =
         scenario {  
-                    do! Scenario.LiftStackFrame
+            do! Scenario.LiftStackFrame
 
-                    let proc = new Process()
-                    do! Scenario.SetCleanupAction (fun () -> proc.Dispose())
-                    proc.StartInfo <- new ProcessStartInfo(exePath)
-                    if proc.Start() then
-                        do! Scenario.SetCleanupAction (fun () -> proc.Kill())
-                        if proc.WaitForInputIdle() then
-                            do! Scenario.SetVariable State_Process proc
-                            return proc.Id
-                        else
-                            return! Scenario.Raise ("Failed to start as application never went idle: " + exePath)
-                    else
-                        return! Scenario.Raise ("Failed to start: " + exePath)
-                    }
+            let proc = new Process()
+            do! Scenario.SetCleanupAction (fun () -> proc.Dispose())
+            proc.StartInfo <- new ProcessStartInfo(exePath)
+            if proc.Start() then
+                do! Scenario.SetCleanupAction (fun () -> proc.Kill())
+                if proc.WaitForInputIdle() then
+                    do! Scenario.SetVariable State_Process proc
+                    return proc.Id
+                else
+                    return! Scenario.Raise ("Failed to start as application never went idle: " + exePath)
+            else
+                return! Scenario.Raise ("Failed to start: " + exePath)
+        }
 
--- fun/mst/mst/UIScenario.fs
+++ fun/mst/mst/UIScenario.fs
@@ -19,61 +19,61 @@ module UIScenario =
 
     let GetCurrentElement : Scenario<AutomationElement> = 
         scenario {
-                    return! Scenario.GetVariable State_Current
-                    }
+            return! Scenario.GetVariable State_Current
+        }
 
     let FindElement (q : Query) : Scenario<AutomationElement option> =     
         scenario {
-                    let! currentElement = GetCurrentElement
-                    let element = currentElement |> DeepFindChild q 
-                    if element = null then
-                        return None
-                    else
-                        return Some element                        
-                    }
+            let! currentElement = GetCurrentElement
+            let element = currentElement |> DeepFindChild q 
+            if element = null then
+                return None
+            else
+                return Some element                        
+        }
 
     let GetElement (q : Query) : Scenario<AutomationElement> =     
         scenario {
-                    let! element = FindElement q
-                    let elem = 
-                        match element with
-                        | Some c    -> c
-                        | _         -> null 
+            let! element = FindElement q
+            let elem = 
+                match element with
+                | Some c    -> c
+                | _         -> null 
                     
-                    if elem = null then
-                        return! Scenario.Raise (sprintf "Element not found: %A" q)
-                    else
-                        return elem
-                    }
+            if elem = null then
+                return! Scenario.Raise (sprintf "Element not found: %A" q)
+            else
+                return elem
+        }
 
     let GetRootElement : Scenario<AutomationElement> = 
         scenario {
-                    return! Scenario.GetVariable State_Window
-                    }
+            return! Scenario.GetVariable State_Window
+        }
 
     let FocusElement (q : Query) : Scenario<unit> =     
         scenario {
-                    let! element = GetElement q
-                    return element.SetFocus()
-                    }
+            let! element = GetElement q
+            return element.SetFocus()
+        }
 
     let FindPropertyValue (q : Query) (ap : AutomationProperty) : Scenario<'T option> =     
         scenario {
-                    let! element = GetElement q
-                    return
-                        match element.GetCurrentPropertyValue(ap) with
-                        | :? 'T as v    -> Some v
-                        | _             -> None
-                    }
+            let! element = GetElement q
+            return
+                match element.GetCurrentPropertyValue(ap) with
+                | :? 'T as v    -> Some v
+                | _             -> None
+        }
 
     let GetPropertyValue (q : Query) (ap : AutomationProperty) : Scenario<'T> =     
         scenario {
-                    let! p = FindPropertyValue q ap
-                    if p.IsSome then
-                        return p.Value
-                    else
-                        return! Scenario.Raise (sprintf "Property not found: %A" ap)
-                    }
+            let! p = FindPropertyValue q ap
+            if p.IsSome then
+                return p.Value
+            else
+                return! Scenario.Raise (sprintf "Property not found: %A" ap)
+        }
 
     let GetBounds       (q : Query) : Scenario<Rect>    = GetPropertyValue q AutomationElementIdentifiers.BoundingRectangleProperty
     let GetName         (q : Query) : Scenario<string>  = GetPropertyValue q AutomationElementIdentifiers.NameProperty
@@ -81,13 +81,13 @@ module UIScenario =
 
     let GetPattern (q : Query) (p : AutomationPattern) : Scenario<#BasePattern> =     
         scenario {
-                    let! element = GetElement q
-                    let o = ref Unchecked.defaultof<obj>
-                    if element.TryGetCurrentPattern(p, o) then
-                        return downcast !o
-                    else
-                        return! Scenario.Raise (sprintf "Element with requested pattern not found: %A" q)
-                    }
+            let! element = GetElement q
+            let o = ref Unchecked.defaultof<obj>
+            if element.TryGetCurrentPattern(p, o) then
+                return downcast !o
+            else
+                return! Scenario.Raise (sprintf "Element with requested pattern not found: %A" q)
+        }
 
     let GetInvokePattern (q : Query) : Scenario<InvokePattern> = GetPattern q InvokePattern.Pattern    
 
@@ -95,83 +95,82 @@ module UIScenario =
 
     let Invoke (q : Query) : Scenario<unit> =     
         scenario {
-                    let! pattern = GetInvokePattern q
-                    ignore <| pattern.Invoke()
-                    return ()
-                    }
+            let! pattern = GetInvokePattern q
+            ignore <| pattern.Invoke()
+            return ()
+        }
 
     let GetText (q : Query) : Scenario<string> =     
         scenario {
-                    let! pattern = GetTextPattern q
-                    return pattern.DocumentRange.GetText(-1)
-                    }
+            let! pattern = GetTextPattern q
+            return pattern.DocumentRange.GetText(-1)
+        }
 
     let SendText (s : string) : Scenario<unit> =
         scenario {
-                    ignore <| Keyboard.Send (s)
+            ignore <| Keyboard.Send (s)
                     
-                    return ()
-                    }
+            return ()
+        }
 
     let SendChar (ch : char) (m : Modifier) : Scenario<unit> =
         scenario {
-                    ignore <| Keyboard.Send (ch, m)
+            ignore <| Keyboard.Send (ch, m)
                     
-                    return ()
-                    }
+            return ()
+        }
 
     let DoMouseGesture (gs : MouseGesture list) : Scenario<unit> =
+        let round (v : float) = int <| Math.Round(v)
+        let apply (gestures : MouseGesture list) = 
+            let mutable p       = Point()
+            let mutable left    = false
+
+            for g in gestures do
+                match g with
+                | LeftClick pp          ->  left <- false
+                                            p <- pp
+                                            ignore <| Mouse.LeftClick(round p.X, round p.Y)
+                | LeftClickAndHold pp   ->  left <- true
+                                            p <- pp
+                                            ignore <| Mouse.LeftClickAndHold(round p.X, round p.Y)
+                | ReleaseLeft pp        ->  left <- false
+                                            p <- pp
+                                            ignore <| Mouse.ReleaseLeft(round p.X, round p.Y)
+                | MoveTo pp             ->  p <- pp
+                                            ignore <| Mouse.MoveTo(round p.X, round p.Y)
+
+            if (left) then ignore <| Mouse.ReleaseLeft(round p.X, round p.Y)
         scenario {
-                    let round (v : float) = int <| Math.Round(v)
-                    let apply (gestures : MouseGesture list) = 
-                        let mutable p       = Point()
-                        let mutable left    = false
-
-                        for g in gestures do
-                            match g with
-                            | LeftClick pp          -> left <- false
-                                                       p <- pp
-                                                       ignore <| Mouse.LeftClick(round p.X, round p.Y)
-                            | LeftClickAndHold pp   -> left <- true
-                                                       p <- pp
-                                                       ignore <| Mouse.LeftClickAndHold(round p.X, round p.Y)
-                            | ReleaseLeft pp        -> left <- false
-                                                       p <- pp
-                                                       ignore <| Mouse.ReleaseLeft(round p.X, round p.Y)
-                            | MoveTo pp             -> p <- pp
-                                                       ignore <| Mouse.MoveTo(round p.X, round p.Y)
-
-                        if (left) then ignore <| Mouse.ReleaseLeft(round p.X, round p.Y)
-
-
-                    return apply gs
-                    }
+            return apply gs
+        }
 
     let SetRootElement (q : Query) : Scenario<unit> =
         scenario {
-                    do! Scenario.LiftStackFrame
+            do! Scenario.LiftStackFrame
 
-                    let root = AutomationElement.RootElement |> ShallowFindChild q
-                    if root = null then
-                        do! Scenario.Raise (sprintf "RootElement not found: %A" q)
-                    else
-                        root.SetFocus();
-                        do! Scenario.SetVariable State_Window root
-                        do! Scenario.SetVariable State_Current root
-                    }
+            let root = AutomationElement.RootElement |> ShallowFindChild q
+            if root = null then
+                do! Scenario.Raise (sprintf "RootElement not found: %A" q)
+            else
+                root.SetFocus();
+                do! Scenario.SetVariable State_Window root
+                do! Scenario.SetVariable State_Current root
+        }
 
     let SetCurrentElement (q : Query) : Scenario<unit> =     
         scenario {
-                    do! Scenario.LiftStackFrame
+            do! Scenario.LiftStackFrame
 
-                    let! element = GetElement q
-                    do! Scenario.SetVariable State_Current element
-                    }
+            let! element = GetElement q
+            do! Scenario.SetVariable State_Current element
+        }
 
-    let StartWindowedProcess exePath = scenario {
-        do! Scenario.LiftStackFrame
+    let StartWindowedProcess exePath = 
+        scenario {
+            do! Scenario.LiftStackFrame
 
-        let! pid = ProcessScenario.StartProcess exePath
-        do! Scenario.Retry 10 500 (SetRootElement <| ByProcessId pid)
+            let! pid = ProcessScenario.StartProcess exePath
+            do! Scenario.Retry 10 500 (SetRootElement <| ByProcessId pid)
         }
     
