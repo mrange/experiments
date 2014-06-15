c7f05745f7f1a957e05e1cf2eec7e8fcf73f46a8
mrange <marten_range@hotmail.com>
2014-06-15 13:22:12 +0200

    Turtles

--- fun/turtle/turtle/Program.fs
+++ fun/turtle/turtle/Program.fs
@@ -6,10 +6,15 @@ open TurtlePower
 [<STAThread>]
 [<EntryPoint>]
 let main argv = 
-    let turtleGenerator = Box.Generate 500.F
-    let turtleGenerator = RecursiveBox.Generate 500.F
-    let turtleGenerator = TreeFractal.Generate 10 250.F
     let turtleGenerator = OptimizedTreeFractal.Generate 10 250.F
+    let turtleGenerator = OtherTreeFractal.Generate 8 250.F
+    let turtleGenerator = RecursiveBox.Generate 500.F
+    let turtleGenerator = SimpleBox.Generate 500.F
+    let turtleGenerator = Box.Generate 500.F
+    let turtleGenerator = SimpleBox.Generate 500.F
+    let turtleGenerator = SimpleTreeFractal.Generate 8 250.F
+    let turtleGenerator = TreeFractal.Generate 10 250.F
+    let turtleGenerator = WavingTreeFractal.Generate 10 250.F
 
     TurtleWindow.Show turtleGenerator
 
--- fun/turtle/turtle/Samples.fs
+++ fun/turtle/turtle/Samples.fs
@@ -2,7 +2,101 @@
 
 open Turtle
 
-module TreeFractal =
+module SimpleBox = 
+
+    let Generate v time = 
+        turtle {
+            do! Forward v
+            do! Forward v
+            do! Turn 90.F
+            do! Forward v
+            do! Turn 90.F
+            do! Forward v
+            do! Turn 90.F
+            do! Forward v
+        }
+
+module Box = 
+
+    let Generate v time = 
+        turtle {
+            do! Forward v
+            do! Forward v
+            for i in 0..2 do
+                do! Turn 90.F
+                do! Forward v
+        }
+        
+module RecursiveBox = 
+
+    let Generate v time = 
+        turtle {
+//            let d = v + 50.0F * (sin <| 20.F * Deg2Rad * time)
+            let d = v
+            do! Forward v
+            do! Forward v
+            for i in 0..200 do
+                do! Turn -88.F
+                let divisor = pown 1.02F i
+                do! Forward <| d / divisor
+        }
+
+
+module SimpleTreeFractal = 
+
+    let rec Generate n v time = 
+        turtle {
+            if n <= 0 then
+                return ()
+            else
+                do! Width 6.0F
+
+                // Step 1
+                do! Forward v
+
+                // Step 2
+                do! Turn 30.0F
+                do! Generate (n - 1) (v * 0.75F) time
+                do! Turn -30.0F
+
+                // Step 3
+                do! Turn -30.0F
+                do! Generate (n - 1) (v * 0.75F) time
+                do! Turn 30.0F
+
+                do! Forward -v
+        }
+
+module TreeFractal = 
+
+    let rec GenerateBranch n v time a = 
+        turtle {
+            do! Turn a
+            do! Generate (n - 1) v time
+        }
+    and Generate n v time = 
+        turtle {
+            if n <= 0 then
+                return ()
+            else
+                let c = 
+                    match n with
+                    | nn when nn < 3    -> Lime
+                    | nn when nn < 6    -> LimeGreen
+                    | _                 -> Brown
+
+                do! Color c
+                do! Width <| float32 n
+
+                //let turn = 30.F
+                let turn = (3.0F * time) % 180.0F
+
+                do! Forward v
+                do! RunAndReturn <| GenerateBranch n (v * 0.75F) time turn
+                do! GenerateBranch n (v * 0.75F) time -turn
+        }
+
+module WavingTreeFractal = 
 
     let LeftScaling     = 1.F / sqrt 2.F
     let RightScaling    = 1.1F * LeftScaling
@@ -31,7 +125,7 @@ module TreeFractal =
             if n <= 0 then
                 do! GenerateFlower v
             else
-                let a = min 20.F <| 1.F + time
+                let turn = min 20.F <| 1.F + time
                 let c = 
                     match n with
                     | nn when nn < 3    -> Lime
@@ -42,35 +136,10 @@ module TreeFractal =
                 do! Turn <| 10.F * (sin <| 20.F * Deg2Rad * time)
                 do! Width <| float32 n
                 do! Forward v
-                do! RunAndReturn <| GenerateBranch n (v * LeftScaling) time a
-                do! GenerateBranch n (v * RightScaling) time -a
+                do! RunAndReturn <| GenerateBranch n (v * LeftScaling) time turn
+                do! GenerateBranch n (v * RightScaling) time -turn
         }
 
-module RecursiveBox = 
-
-    let Generate v time = 
-        turtle {
-            let d = v + 50.0F * (sin <| 20.F * Deg2Rad * time)
-            do! Forward v
-            do! Forward v
-            for i in 0..200 do
-                do! Turn -88.F
-                let divisor = pown 1.02F i
-                do! Forward <| d / divisor
-        }
-
-
-module Box = 
-
-    let Generate v time = 
-        turtle {
-            do! Forward v
-            do! Forward v
-            for i in 0..3 do
-                do! Turn 90.F
-                do! Forward v
-        }
-        
 
 module OptimizedTreeFractal =
 
@@ -97,7 +166,7 @@ module OptimizedTreeFractal =
         if n <= 0 then
             GenerateFlower v
         else
-            let a = min 20.F <| 1.F + time
+            let turn = min 20.F <| 1.F + time
             let c = 
                 match n with
                 | nn when nn < 3    -> Lime
@@ -109,5 +178,40 @@ module OptimizedTreeFractal =
             >>+ Turn turn
             >>+ Width (float32 n)
             >>+ Forward v
-            >>+ RunAndReturn (GenerateBranch n (v * LeftScaling) time a)
-            >>+ GenerateBranch n (v * RightScaling) time -a
+            >>+ RunAndReturn (GenerateBranch n (v * LeftScaling) time turn)
+            >>+ GenerateBranch n (v * RightScaling) time -turn
+
+module OtherTreeFractal =
+
+    let LeftScaling     = 1.F / sqrt 2.F
+    let RightScaling    = 1.1F * LeftScaling
+
+    let rec GenerateBranch n v time a = 
+        turtle {
+            do! Turn a
+            do! Generate (n - 1) v time
+        }
+    and Generate n v time = 
+        turtle {
+            if n <= 0 then
+                return ()
+            else
+                let c = 
+                    match n with
+                    | nn when nn < 3    -> Lime
+                    | nn when nn < 6    -> LimeGreen
+                    | _                 -> Brown
+
+                do! Color c
+                do! Width <| float32 n
+
+                do! Forward <| v
+                do! RunAndReturn <| GenerateBranch n (v * 0.7F) time -30.F
+
+                do! Forward <| v / 2.0F
+                do! RunAndReturn <| GenerateBranch n (v * 0.5F) time 25.F
+
+                do! Forward <| v
+                do! RunAndReturn <| GenerateBranch n (v * 0.5F) time 25.F
+        }
+
