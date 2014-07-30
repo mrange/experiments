53fd18707423a8b9d4f66f70ebd29d2b9e89bdf4
mrange <marten_range@hotmail.com>
2014-07-30 20:36:48 +0200

    More gravity

--- fun/gravity/gravity/Gravity.fs
+++ fun/gravity/gravity/Gravity.fs
@@ -14,6 +14,7 @@ namespace GravitySucks
 
 module Gravity =
     open SharpDX
+    open Spatial
     open System
 
     let M = 10.F
@@ -42,7 +43,16 @@ module Gravity =
             mutable Radius      : float32
             mutable Current     : Vector2
             mutable Velocity    : Vector2
+            mutable Index       : int
+            mutable Leaf        : QuadLeaf
         }
+
+        interface ISpatialObject with
+            member x.Pos        = x.Current
+            member x.Location  
+                with get ()     = x.Index,x.Leaf
+                and  set (i,l)  = x.Index <- i; x.Leaf <- l
+            
 
         member x.UpdateRadius ()                = x.Radius <- float32 <| Math.Pow (float x.Mass, 1.0/3.0)
 
@@ -67,6 +77,8 @@ module Gravity =
                 Radius      = float32 <| Math.Pow (float m, 1.0/3.0)
                 Current     = c 
                 Velocity    = v
+                Index       = -1
+                Leaf        = null
             }
 
     [<Struct>]
@@ -77,28 +89,36 @@ module Gravity =
         member x.Current    = current
         member x.Velocity   = velocity
 
-    let ApplyGravity (timeStep : float32) (particles : Particle []) =
-        let last = particles.Length - 1
-        for o in 0..last do
-            let outer           = particles.[o]
-            for i in o + 1..last do
-                let inner       = particles.[i]
-                let diff        = outer.Current - inner.Current
-                diff.Normalize ()
-                let force       = outer.GravityForce inner
-                outer.Velocity  <-outer.Velocity - timeStep * force*diff / outer.Mass
-                inner.Velocity  <-inner.Velocity + timeStep * force*diff / inner.Mass
-        particles
+    type ParticleTree = QuadRoot<Particle>
 
-    let ApplyInertia (timeStep : float32) (particles : Particle []) = 
-        let last = particles.Length - 1
-        for p in 0..last do
-            let particle        = particles.[p]
+    let ApplyGravity (timeStep : float32) (tree : ParticleTree) =
+        let last = tree.ObjectLength - 1
+        for i in 0..last do
+            let particle        = tree.Object i
+            let leaf            = particle.Leaf
+            let last            = leaf.ChildCount - 1
+
+            for i in 0..last do
+                let other       = leaf.Object<Particle> i
+                if other <> particle then
+                    let diff            = particle.Current - other.Current
+                    diff.Normalize ()
+                    let force           = particle.GravityForce other
+                    let v               = timeStep*force*diff
+                    particle.Velocity   <-particle.Velocity - v / particle.Mass
+                    other.Velocity      <-other.Velocity + v / other.Mass
+
+        tree
+
+    let ApplyInertia (timeStep : float32) (tree : ParticleTree) = 
+        let last = tree.ObjectLength - 1
+        for i in 0..last do
+            let particle        = tree.Object i
             let np              = particle.Current + timeStep * particle.Velocity
             particle.Current    <- np
-        particles
+        tree
 
-    let ApplyCollision (particles : Particle []) = 
+    let ApplyCollision (tree : ParticleTree) = 
         let last = particles.Length - 1
         for o in 0..last do
             let outer           = particles.[o]
@@ -122,26 +142,38 @@ module Gravity =
                 i <- i + 1
         particles
 
-    let RemoveMassless (particles : Particle []) =
-        particles |> Array.filter (fun p -> p.Mass > 0.F)
+    let RemoveMassless (tree : ParticleTree) =
+        tree.InplaceFilter <| fun p -> p.Mass > 0.F 
+        tree
 
-    let ToRenderParticles (particles : Particle []) =
-        [|
-            for p in particles -> RenderParticle(p.Mass, p.Radius, p.Current, p.Velocity)
-        |]
+    let UpdateTree (tree : ParticleTree) = 
+        tree.Update ()
+        tree.Trim ()
+        tree
 
-    let Cleanup (particles : Particle []) = 
-        particles
-        |> ApplyCollision  
+    let ToRenderParticles (tree : ParticleTree) =
+        let length  = tree.ObjectLength
+        let rps     = Array.zeroCreate<RenderParticle> length
+        for i in 0..length - 1 do
+            let p   = tree.Object i
+            let rp  = RenderParticle(p.Mass, p.Radius, p.Current, p.Velocity)
+            rps.[i] <- rp
+        rps
+
+    let Cleanup (tree : ParticleTree) = 
+        tree
+        |> ApplyCollision
         |> RemoveMassless
+        |> UpdateTree
 
-    let TimeStep (timeStep : float32) (particles : Particle []) =
-        let particles = particles
-                        |> ApplyGravity    timeStep 
-                        |> ApplyInertia    timeStep 
-                        |> ApplyCollision  
-                        |> RemoveMassless
-        particles, ToRenderParticles particles
+    let TimeStep (timeStep : float32) (tree : ParticleTree) =
+        let tree =  tree
+                    |> ApplyGravity    timeStep
+                    |> ApplyInertia    timeStep 
+                    |> ApplyCollision  
+                    |> RemoveMassless  
+                    |> UpdateTree
+        tree, ToRenderParticles tree
 
 
 
--- fun/gravity/gravity/Spatial.fs
+++ fun/gravity/gravity/Spatial.fs
@@ -15,6 +15,7 @@ namespace GravitySucks
 module Spatial = 
     open SharpDX
     open System
+    open System.Linq
 
     type Bounds = RectangleF
 
@@ -33,19 +34,21 @@ module Spatial =
 
 
     [<AllowNullLiteral>]
-    [<AbstractClass>]
     type ISpatialObject =
+        abstract Mass       : float32
         abstract Pos        : Vector2
         abstract Location   : int*QuadLeaf  with get,set
 
     and [<AllowNullLiteral>] [<AbstractClass>] QuadTree(bounds : Bounds) as this = 
-
-        [<DefaultValue>] val mutable childCount : int
-        [<DefaultValue>] val mutable center     : Vector2
-        [<DefaultValue>] val mutable parent     : QuadJunction
+        [<DefaultValue>] val mutable internal childCount : int
+        [<DefaultValue>] val mutable internal center     : Vector2
+        [<DefaultValue>] val mutable internal parent     : QuadJunction
+        [<DefaultValue>] val mutable internal totalMass  : float32
 
         do 
             this.center <- bounds.Center
+
+        member inline x.TotalMass = x.totalMass
 
         member inline x.ChildCount  
             with get () = x.childCount
@@ -56,56 +59,58 @@ module Spatial =
 
         member inline x.Parent
             with get () = x.parent
-            and  set v  = x.parent <- v
 
-        abstract Add    : Vector2*ISpatialObject    -> QuadTree
-        abstract Update : unit                      -> unit
-        abstract Trim   : unit                      -> QuadTree
+        abstract Add    : float32*Vector2*ISpatialObject    -> QuadTree
+        abstract Update : unit                              -> unit
+        abstract Trim   : unit                              -> QuadTree
 
     and [<AllowNullLiteral>] [<Sealed>] QuadJunction(b : Bounds) = 
         inherit QuadTree(b)
     
-        [<DefaultValue>] val mutable c00 : QuadTree
-        [<DefaultValue>] val mutable c10 : QuadTree
-        [<DefaultValue>] val mutable c01 : QuadTree
-        [<DefaultValue>] val mutable c11 : QuadTree
+        [<DefaultValue>] val mutable private c00 : QuadTree
+        [<DefaultValue>] val mutable private c10 : QuadTree
+        [<DefaultValue>] val mutable private c01 : QuadTree
+        [<DefaultValue>] val mutable private c11 : QuadTree
 
         new (b,l00,l10,l01,l11) as x =
             QuadJunction(b) then
-            x.c00 <- l00
-            x.c10 <- l10
-            x.c01 <- l01
-            x.c11 <- l11
-            x.c00.Parent <- x
-            x.c10.Parent <- x
-            x.c01.Parent <- x
-            x.c11.Parent <- x
+            x.c00           <- l00
+            x.c10           <- l10
+            x.c01           <- l01
+            x.c11           <- l11
+            x.c00.parent    <- x
+            x.c10.parent    <- x
+            x.c01.parent    <- x
+            x.c11.parent    <- x
+            x.totalMass     <- x.c00.totalMass + x.c10.totalMass + x.c01.totalMass + x.c11.totalMass
 
-        override x.Add (p : Vector2, so : ISpatialObject) =
-            let c = x.Center
+        override x.Add (m : float32, p : Vector2, so : ISpatialObject) =
+            let c       = x.center
 
             if p.X < c.X && p.Y < c.X then
-                x.c00 <- x.c00.Add (p,so)
+                x.c00   <- x.c00.Add (m,p,so)
             elif p.Y < c.Y then
-                x.c10 <- x.c10.Add (p,so)
+                x.c10   <- x.c10.Add (m,p,so)
             elif p.X < c.X then
-                x.c01 <- x.c01.Add (p,so)
+                x.c01   <- x.c01.Add (m,p,so)
             else
-                x.c11 <- x.c11.Add (p,so)
+                x.c11   <- x.c11.Add (m,p,so)
 
-            x.ChildCount <- x.ChildCount + 1
+            x.childCount<- x.childCount + 1
+
+            x.totalMass <- x.totalMass + m
 
             upcast x
 
         override x.Update () = 
-            if x.ChildCount > 0 then
+            if x.childCount > 0 then
                 x.c00.Update ()
                 x.c10.Update ()
                 x.c01.Update ()
                 x.c11.Update ()
 
         override x.Trim () = 
-            if x.ChildCount > 0 then
+            if x.childCount > 0 then
                 x.c00 <- x.c00.Trim ()
                 x.c10 <- x.c10.Trim ()
                 x.c01 <- x.c01.Trim ()
@@ -118,14 +123,14 @@ module Spatial =
     and [<AllowNullLiteral>] [<Sealed>] QuadLeaf(b : Bounds) =
         inherit QuadTree(b)
 
-        [<DefaultValue>] val mutable so0 : ISpatialObject
-        [<DefaultValue>] val mutable so1 : ISpatialObject
-        [<DefaultValue>] val mutable so2 : ISpatialObject
-        [<DefaultValue>] val mutable so3 : ISpatialObject
-        [<DefaultValue>] val mutable so4 : ISpatialObject
-        [<DefaultValue>] val mutable so5 : ISpatialObject
-        [<DefaultValue>] val mutable so6 : ISpatialObject
-        [<DefaultValue>] val mutable so7 : ISpatialObject
+        [<DefaultValue>] val mutable private so0 : ISpatialObject
+        [<DefaultValue>] val mutable private so1 : ISpatialObject
+        [<DefaultValue>] val mutable private so2 : ISpatialObject
+        [<DefaultValue>] val mutable private so3 : ISpatialObject
+        [<DefaultValue>] val mutable private so4 : ISpatialObject
+        [<DefaultValue>] val mutable private so5 : ISpatialObject
+        [<DefaultValue>] val mutable private so6 : ISpatialObject
+        [<DefaultValue>] val mutable private so7 : ISpatialObject
 
         member private x.Child 
             with get i : ISpatialObject =
@@ -151,12 +156,13 @@ module Spatial =
                 | 7 -> x.so7 <- v
                 | _ -> raise <| IndexOutOfRangeException()
 
+        member x.Object i : #ISpatialObject = downcast x.Child i 
 
-        override x.Add (p : Vector2, so : ISpatialObject) =
-            let cnt = x.ChildCount
+        override x.Add (m : float32, p : Vector2, so : ISpatialObject) =
+            let cnt = x.childCount
             if cnt < ChildrenInLeaf then
                 let b = x.Bounds
-                let c = x.Center
+                let c = x.center
                 let b00,b10,b01,b11 = CutBounds b
                 let l00,l10,l01,l11 = QuadLeaf(b00),QuadLeaf(b10),QuadLeaf(b01),QuadLeaf(b11)
 
@@ -165,48 +171,92 @@ module Spatial =
                     if so <> null then
                         let p = so.Pos
                         if p.X < c.X && p.Y < c.X then
-                            ignore <| l00.Add (p,so)
+                            ignore <| l00.Add (m, p,so)
                         elif p.Y < c.Y then
-                            ignore <| l10.Add (p,so)
+                            ignore <| l10.Add (m, p,so)
                         elif p.X < c.X then
-                            ignore <| l01.Add (p,so)
+                            ignore <| l01.Add (m, p,so)
                         else
-                            ignore <| l11.Add (p,so)
+                            ignore <| l11.Add (m, p,so)
 
                 let j = QuadJunction(b,l00,l10,l01,l11)                               
-                j.Parent <- x.Parent
-                j.Add (p,so)
+                j.parent <- x.parent
+                j.Add (m, p,so)
             else
                 so.Location     <- cnt,x
                 x.Child cnt     <- so
-                x.ChildCount    <- cnt + 1
+                x.childCount    <- cnt + 1
+                x.totalMass     <- x.totalMass + m
                 upcast x
+
+        member internal x.PreRemove (i : int) = 
+            let so                  = x.Child i
+            if so <> null then
+                let m               = so.Mass
+                let p               = so.Pos
+                let mutable parent  = x.parent
+                x.totalMass         <- x.totalMass - m
+
+                while parent <> null && not <| parent.Bounds.Contains p do
+                    parent.childCount   <- x.childCount - 1
+                    parent.totalMass    <- x.totalMass - m
+                    parent              <- x.parent
+
+        member internal x.PostRemove () = 
+            let cnt         = x.childCount            
+            let mutable e   = cnt
+            let mutable i   = e
+
+            while i < e do
+                let so = x.Child i
+                if so = null then
+                    let last        = e - 1
+                    let l           = x.Child last
+                    if l <> null then
+                        l.Location  <- i,x
+                    x.Child i       <- x.Child last
+                    x.Child last    <- so
+                    e               <- last
+                else
+                    i <- i + 1
+
+            x.childCount <- i
 
         override x.Update () = 
             let b               = x.Bounds
-            let cnt             = x.ChildCount
+            let cnt             = x.childCount
             let mutable e       = cnt
             let mutable i       = 0
             while i < e do
                 let so = x.Child i
                 if not <| b.Contains so.Pos then
                     let last        = e - 1
+                    let l           = x.Child last
+                    so.Location     <- -1,null
+                    l.Location      <- i,x
                     x.Child i       <- x.Child last
                     x.Child last    <- so
                     e               <- last
+                else
+                    i <- i + 1
 
-            x.ChildCount <- e
+            x.childCount <- e
         
             while e < cnt do
                 let so              = x.Child e
+                let m               = so.Mass
                 let p               = so.Pos
-                let mutable parent  = x.Parent
+                let mutable parent  = x.parent
+
+                x.totalMass         <- x.totalMass - m
 
                 while parent <> null && not <| parent.Bounds.Contains p do
-                    parent <- x.Parent
+                    parent.childCount   <- x.childCount - 1
+                    parent.totalMass    <- x.totalMass - m
+                    parent              <- x.parent
 
                 if parent <> null then            
-                    ignore <| parent.Add (p,so)            
+                    ignore <| parent.Add (m, p,so)            
                 else
                     raise TodoException
                 
@@ -215,21 +265,24 @@ module Spatial =
         override x.Trim () = 
             upcast x
 
-    type QuadRoot(b : Bounds) as this= 
-        [<DefaultValue>] val mutable tree : QuadTree
+    type QuadRoot<'T when 'T :> ISpatialObject>(b : Bounds) as this= 
+        [<DefaultValue>] val mutable private tree   : QuadTree
+        [<DefaultValue>] val mutable private objects: ResizeArray<'T>
 
         do
-            this.tree <- QuadLeaf(b : Bounds)
-
+            this.tree       <- QuadLeaf(b : Bounds)
+            this.objects    <- ResizeArray<'T>(32)
 
         let leaf (b : Bounds) : QuadTree = upcast QuadLeaf(b)
 
+        member x.ObjectLength   = x.objects.Count
+        member x.Object i       = x.objects.[i]
 
         member private x.ExpandBounds (p : Vector2) =
             let b = x.tree.Bounds
             if not <| b.Contains p then
                 let inline cb x y   = Bounds(x, y, 2.F * b.Width , 2.F * b.Height)
-                let c               = x.tree.Center
+                let c               = x.tree.center
                 let b,l00,l10,l01,l11 = 
                     if p.X < c.X && p.Y < c.X then
                         let b = cb (b.X - b.Width) (b.Y - b.Height)
@@ -250,11 +303,50 @@ module Spatial =
                 x.tree <- QuadJunction (b,l00,l10,l01,l11)
                 x.ExpandBounds p
 
-        member x.Add (so : ISpatialObject) =
-            if so <> null then
+        member x.Add (so : 'T) =
+            if not <| obj.ReferenceEquals (so, null) then
+                let m = so.Mass
                 let p = so.Pos
                 let b = x.tree.Bounds
                 if not <| b.Contains p then
                     x.ExpandBounds p
-                x.tree <- x.tree.Add (p, so)
+                x.tree <- x.tree.Add (m, p, so)
+                x.objects.Add so
+
+        member x.InplaceFilter (filter : 'T -> bool) =
+            let objs        = x.objects
+            let cnt         = objs.Count 
+            let mutable e   = cnt
+            let mutable i   = 0
+
+            while i < e do
+                let o = objs.[i]
+                if not <| filter o then
+                    let last = e - 1
+                    objs.[i]    <- objs.[last]
+                    objs.[i]    <- o
+                    e           <- last
+                else
+                    i <- i + 1
+
+            let leafs = ResizeArray<QuadLeaf>(cnt - i)
+
+            while i < cnt do
+                let o       = objs.[i]
+                let i,l     = o.Location
+                leafs.Add l
+                l.PreRemove i
+                e <- e + 1
+
+            let leafs = leafs.Distinct().ToArray()
+            for leaf in leafs do
+                leaf.PostRemove()
+
+            objs.RemoveRange (e, cnt - e)
+
+        member x.Update () = 
+            x.tree.Update ()
+
+        member x.Trim () = 
+            ignore <| x.tree.Trim ()
 
