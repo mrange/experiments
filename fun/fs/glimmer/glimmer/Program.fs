b3f3725530b6a03f97d030103ffef456e7189ed1
mrange <marten_range@hotmail.com>
2014-10-08 20:11:42 +0200

    glimmer

---
+++ fun/fs/glimmer/glimmer/Program.fs
@@ -0,0 +1,43 @@
+﻿
+module glimmer =
+
+    module model =
+        
+        type DataType =
+        |   Decimal
+        |   String
+
+        type ColumDefinition(id : string, cdt : DataType) =
+
+            member x.Id         = id
+            member x.DataType   = cdt
+
+        type TableDefinition(id : string, columns : ColumDefinition[]) =
+
+            member x.Id         = id
+            member x.Columns    = columns
+
+    module data =
+        
+        open model
+
+        type Row(table : Table) = 
+
+            member x.Table = table
+
+        and Table(tableDefinition : TableDefinition) = 
+
+            member x.TableDefinition    = tableDefinition
+
+
+    
+    module extension =
+
+        type Test() = 
+            
+            member x.Tester() = ()
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0
