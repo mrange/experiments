77666657cee9efe6215cae012c1acdac2bf3f1a3
mrange <marten_range@hotmail.com>
2014-11-01 19:57:05 +0100

    Async basics in place

--- c++/cppsync/cppsync/cppsync.cpp
+++ c++/cppsync/cppsync/cppsync.cpp
@@ -26,21 +26,20 @@ using on_error      = std::function<void (std::exception const &)>;
 using on_cancelled  = std::function<void (cancel_reason)>       ;
 
 template<typename TValue>
-using async             = std::function<void (
-    async_context       &       ,
-    on_value<TValue>    const & ,
-    on_error            const & ,
-    on_cancelled        const & )>;
-
-
-template<typename TA, typename TB>
-using binder    = std::function<async<TB> (TA &&)>;
+struct async;
 
 namespace details
 {
+    template<typename TValue>
+    using async_func        = std::function<void (
+        async_context       &       ,
+        on_value<TValue>    const & ,
+        on_error            const & ,
+        on_cancelled        const & )>;
+
     template<typename T>
     struct deduce_async_type;
-    
+
     template<typename T>
     struct deduce_async_type<async<T>>
     {
@@ -56,7 +55,7 @@ namespace details
             value   = false ,
         };
     };
-    
+
     template<typename T>
     struct is_async<async<T>>
     {
@@ -80,6 +79,49 @@ namespace details
 }
 
 template<typename TValue>
+struct async
+{
+    using value_type = TValue;
+
+    details::async_func<TValue>  func ;
+
+    async (details::async_func<TValue> func)
+        :   func (std::move (func))
+    {
+    }
+
+    void execute (
+            async_context       &       ctx
+        ,   on_value<TValue>    const & ov
+        ,   on_error            const & oe
+        ,   on_cancelled        const & oc
+        )
+    {
+        func (ctx, ov, oe, oc);
+    }
+
+    template<typename TBinder>
+    typename details::deduce_binder_return_type<TValue, TBinder>::type bind (TBinder && binder)
+    {
+        return
+            [inner = *this, ibinder = std::forward<TBinder> (binder)]
+            (async_context & ctx, on_value<TValue> const & ov, on_error const & oe, on_cancelled const & oc) mutable
+            {
+                auto fov = [&ctx, &ov, &oe, &oc, iibinder = std::move (ibinder)] (TValue && v)
+                {
+                    auto b = iibinder (std::move (v));
+                    b.execute (ctx, ov, oe, oc);
+                };
+
+                inner.execute (ctx, fov, oe, oc);
+            };
+    }
+};
+
+template<typename TA, typename TB>
+using binder    = std::function<async<TB> (TA &&)>;
+
+template<typename TValue>
 async<TValue> async_return (TValue && value)
 {
     return
@@ -100,17 +142,20 @@ typename details::deduce_binder_return_type<TA, TBinder>::type async_bind (async
             auto fov = [&ctx, &ov, &oe, &oc, iibinder = std::move (ibinder)] (TA && v)
             {
                 auto b = iibinder (std::move (v));
-                b (ctx, ov, oe, oc);
+                b.execute (ctx, ov, oe, oc);
             };
 
-            ia (ctx, fov, oe, oc);
+            ia.execute (ctx, fov, oe, oc);
         };
 }
 
 int main ()
 {
     auto a = async_return (std::string ("Test"));
-    auto b = async_bind (a, [] (auto a) { return async_return ("Testing_" + a); });
+//    auto b = async_bind (a, [] (auto a) { return async_return ("Testing_" + a); });
+    auto b = a
+        .bind ([] (auto a) { return async_return ("Testing_" + a); })
+        .bind ([] (auto a) { return async_return ("Testing_" + a); });
 
     async_context ctx;
 
@@ -129,7 +174,7 @@ int main ()
         printf ("Cancelled: %d\n", cr);
     };
 
-    b (ctx, on_string, on_error, on_cancelled);
+    b.execute (ctx, on_string, on_error, on_cancelled);
 
     return 0;
 }
