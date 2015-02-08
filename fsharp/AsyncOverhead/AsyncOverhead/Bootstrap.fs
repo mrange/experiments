be2fd8eab994257c58fe98111982c26c1066ad59
mrange <marten_range@hotmail.com>
2015-02-08 11:20:42 +0100

    Perf test

---
+++ fsharp/AsyncOverhead/AsyncOverhead/Bootstrap.fs
@@ -0,0 +1,15 @@
+﻿namespace Overhead.Microsoft.FSharp.Control
+
+module SR =
+    let controlContinuationInvokedMultipleTimes     = "controlContinuationInvokedMultipleTimes"
+    let mismatchIAREnd                              = "mismatchIAREnd"
+    let mismatchIARCancel                           = "mismatchIARCancel"
+    let failedReadEnoughBytes                       = "failedReadEnoughBytes"
+    let mailboxReceiveTimedOut                      = "mailboxReceiveTimedOut"
+    let mailboxScanTimedOut                         = "mailboxScanTimedOut"
+    let mailboxProcessorAlreadyStarted              = "mailboxProcessorAlreadyStarted"
+    let mailboxProcessorPostAndReplyTimedOut        = "mailboxProcessorPostAndReplyTimedOut"
+    let mailboxProcessorPostAndAsyncReplyTimedOut   = "mailboxProcessorPostAndAsyncReplyTimedOut"
+    
+    let GetString (k : string) = k
+
---
+++ fsharp/AsyncOverhead/AsyncOverhead/Program.fs
@@ -0,0 +1,75 @@
+﻿module PerfTest = 
+    open System.Diagnostics
+
+    let measurePerformance (count : int) (action : unit -> 'T) =
+        
+        let sw = Stopwatch()
+        sw.Start ()
+
+        let result = action ()
+
+        for i in 2..count do
+            ignore <| action ()
+
+        sw.Stop ()
+
+        result, sw.ElapsedMilliseconds
+
+module AsyncTest = 
+    let rec fib n =
+        async {
+          if n < 2L then
+            return n
+          else
+            let! n1 = fib (n-1L)
+            let! n2 = fib (n-2L)
+            return n1 + n2
+        }
+
+    let test () = Async.RunSynchronously (fib 23L)
+
+module OverheadAsyncTest = 
+
+    let async2 = new Overhead.Microsoft.FSharp.Control.AsyncBuilder()
+
+    let rec fib n =
+        async2 {
+          if n < 2L then
+            return n
+          else
+            let! n1 = fib (n-1L)
+            let! n2 = fib (n-2L)
+            return n1 + n2
+        }
+
+    let test () = Overhead.Microsoft.FSharp.Control.Async.RunSynchronously (fib 23L)
+
+module ControlTest = 
+
+    let async2 = new Overhead.Microsoft.FSharp.Control.AsyncBuilder()
+
+    let rec fib n =
+        if n < 2L then n
+        else
+            let n1 = fib (n-1L)
+            let n2 = fib (n-2L)
+            n1 + n2
+
+    let test () = fib 23L
+
+[<EntryPoint>]
+let main argv = 
+
+    let tests =
+        [|
+            "control"   , ControlTest.test
+            "async"     , AsyncTest.test
+            "async2"    , OverheadAsyncTest.test
+        |]
+
+    for name, action in tests do
+        let v, ms = PerfTest.measurePerformance 10 action
+
+        printfn "%A, result: %A, elapsed: %A" name v ms
+
+    0
