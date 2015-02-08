477978a029a20712765c8dc764e7b4b7ff652e02
mrange <marten_range@hotmail.com>
2015-02-08 12:38:22 +0100

    More samples

--- fsharp/AsyncOverhead/AsyncOverhead/Program.fs
+++ fsharp/AsyncOverhead/AsyncOverhead/Program.fs
@@ -106,10 +106,11 @@ module Async2Test =
           if n < 2L then
             return n
           else
-            let! n1 = fib (n-1L)
             let! n2 = fib (n-2L)
+            let! n1 = fib (n-1L)
             return n1 + n2
         }
+
 
     let test () = Async.RunSynchronously (fib Limit)
     
@@ -180,7 +181,22 @@ module Async3Test =
 
     let test () = Async.RunSynchronously (fib Limit)
 
+module TaskTest = 
+
+    open System.Threading.Tasks
+    
+    let rec fib n =
+      if n < 2L then n
+      else
+        let n2t = Task.Factory.StartNew (fun _ -> fib (n-2L))
+        let n1 = fib (n-1L)
+        n2t.Result + n1
+ 
+    let test () = fib Limit
+
+
 module AsyncTest = 
+(*
     let rec fib n =
         async {
           if n < 2L then
@@ -190,8 +206,34 @@ module AsyncTest =
             let! n2 = fib (n-2L)
             return n1 + n2
         }
+*)
+
+    let rec fib n =
+        async {
+          if n < 2L then
+            return n
+          else
+            let! n2a = fib (n-2L) |> Async.StartChild
+            let! n1 = fib (n-1L)
+            let! n2 = n2a
+            return n1 + n2
+        }
 
     let test () = Async.RunSynchronously (fib Limit)
+
+module HopacTest =
+    open Hopac
+    open Hopac.Job.Infixes
+
+    let rec fib n = Job.delay <| fun () ->
+      if n < 2L then
+        Job.result n
+      else
+        fib (n-2L) <*> fib (n-1L) |>> fun (x, y) ->
+        x + y
+
+    let test () = run (fib Limit)
+
 
 module OverheadAsyncTest = 
 
@@ -222,13 +264,24 @@ module ControlTest =
 
     let test () = fib Limit
 
+open System.Diagnostics
+open System.Threading
+
 [<EntryPoint>]
 let main argv = 
+
+    let p = Process.GetCurrentProcess ()
+    p.ProcessorAffinity <- nativeint 1
+
+    printfn "Awaiting affinity to stick..."
+    Thread.Sleep 500
 
     let tests =
         [|
             "control"   , ControlTest.test
             "async"     , AsyncTest.test
+            "task"      , TaskTest.test
+            "hopac"     , HopacTest.test
             "asynco"    , OverheadAsyncTest.test
             "async2"    , Async2Test.test
             "async3"    , Async3Test.test
