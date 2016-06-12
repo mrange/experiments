35d8be8b8773d7c16d34f700c6b1c6001a39267b
mrange <marten_range@hotmail.com>
2016-06-12 10:31:40 +0200

    Fixed includes

--- amp/mandelbrot/Mandelbrot2/Mandelbrot/Mandelbrot/Mandelbrot.cpp
+++ amp/mandelbrot/Mandelbrot2/Mandelbrot/Mandelbrot/Mandelbrot.cpp
@@ -13,6 +13,30 @@
 //--------------------------------------------------------------------------------------
 #include "stdafx.h"
 
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <windowsx.h>
+
+#include <cassert>
+#include <cstdio>
+#include <cwchar>
+#include <chrono>
+#include <memory>
+#include <vector>
+
+#include <amp.h>
+#include <amp_graphics.h>
+#include <amp_math.h>
+#include <ppl.h>
+#include <ppltasks.h>
+
+#include <d3d11_2.h>
+#include <directxmath.h>
+#include <directxcolors.h>
+
 //d3d11.lib;d3dcompiler.lib;dxguid.lib;winmm.lib;comctl32.lib;%(AdditionalDependencies)
 
 #pragma comment (lib, "d3d11")
