1c54b0412b60efaf508bd40f9fbf5692d76d8a77
mrange <marten_range@hotmail.com>
2018-05-28 20:45:29 +0200

    Reproduce

--- blazor_bug/blazor/blazor_app/Pages/TestFlazor.cs
+++ blazor_bug/blazor/blazor_app/Pages/TestFlazor.cs
@@ -43,4 +43,40 @@
     }
 
   }
+
+  public static class Reproduce2
+  {
+    public static U Map<T0, T1, T2, T3, U>(
+        Func<T0> t0,
+        Func<T1> t1,
+        Func<T2> t2,
+        Func<T3> t3,
+        Func<T0, T1, T2, T3, U> m)
+    {
+      var t = t0.AndAlso(t1).AndAlso(t2).AndAlso(t3);
+
+      var (((tv0, tv1), tv2), tv3) = t();
+
+      return m(tv0, tv1, tv2, tv3);
+    }
+
+    public static string TestStrings()
+    {
+      var r = Map(() => "1", () => "2", () => "3", () => "4", (x, y, z, w) => (x, y, z, w));
+      return r.ToString();
+    }
+
+    public static string TestInts()
+    {
+      var r = Map(() => 1, () => 2, () => 3, () => 4, (x, y, z, w) => (x, y, z, w));
+      return r.ToString();
+    }
+
+    public static string TestMixed()
+    {
+      var r = Map(() => "1", () => "2", () => 3, () => 4, (x, y, z, w) => (x, y, z, w));
+      return r.ToString();
+    }
+
+  }
 }
